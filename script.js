f21f53e66dc8afe3db35a779eb1da470cb4b6660
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@redge.com>
2023-11-04 22:17:17 +0100

    1.2

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-var btnElem = document.querySelector('.sum-btn');
-var numInput = document.querySelector('.num-input');
-function sum(a, b) {
-    console.log(a + b);
+{
+    var persons_1 = [];
+    function addPerson(firstName, lastName) {
+        var name = firstName + lastName;
+        persons_1.push(name);
+    }
+    var firstName = 'John';
+    var lastName = 'Doe';
+    var age = 18;
+    var person = {
+        firstName: firstName,
+        lastName: lastName
+    };
+    addPerson(person.firstName, person.lastName);
+    addPerson('Amanda', 'Doe');
+    addPerson('Thomas', 'Jefferson');
 }
-btnElem.addEventListener('click', function () { sum(5, +numInput.value); });
--- script.ts
+++ script.ts
@@ -1,8 +1,24 @@
-const btnElem = document.querySelector('.sum-btn') as HTMLInputElement;
-const numInput = document.querySelector('.num-input') as HTMLInputElement;
+{
+  const persons: string[] = [];
 
-function sum(a: number, b: number) {
-  console.log(a + b);
+  function addPerson(firstName: string, lastName: string): void {
+    const name: string = firstName + lastName;
+    persons.push(name);
+  }
+
+  let firstName: string = 'John';
+  let lastName: string = 'Doe';
+  let age: number = 18;
+
+  const person: {
+    firstName: string;
+    lastName: string;
+  } = {
+    firstName,
+    lastName
+  }
+
+  addPerson(person.firstName, person.lastName);
+  addPerson('Amanda', 'Doe');
+  addPerson('Thomas', 'Jefferson');
 }
-
-btnElem.addEventListener('click', () => { sum(5, +numInput.value) });
